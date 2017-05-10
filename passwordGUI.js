5f21e14b3118c26c2167921e83f8d2bcb963b230
Carlos Torres <8ctorres@stpatrick.org>
2017-05-10 09:55:08 -0500

    Update passwordGUI.js
    Removed unnecessary logs

--- passwordGUI.js
+++ passwordGUI.js
@@ -10,37 +10,33 @@ function getString(){
 
     password.question("What would you like to set as your password?", function(str){
         let passesAllChecks = true;
+        console.log("")
         console.log("Cheking password...")
         //Conditional Statements here.
         if(testString.checkLength(str)==false){
           passesAllChecks = false;
         }
-        console.log("GUI ran checkLength. Result of passesAllChecks="+passesAllChecks)
         if(testString.containsUpper(str)==false){
           passesAllChecks = false;
         }
-        console.log("GUI ran containsUpper. Result of passesAllChecks="+passesAllChecks)
         if (testString.containsLower(str)==false) {
           passesAllChecks = false;
         }
-        console.log("GUI ran containsLower Result of passesAllChecks="+passesAllChecks)
         if (testString.containsNumerical(str)==false) {
           passesAllChecks = false;
         }
-        console.log("GUI ran containsNumerical. Result of passesAllChecks="+passesAllChecks)
         if (testString.containsSpecial(str)==false) {
           passesAllchecks = false;
         }
-        console.log("GUI ran containsSpecial. Result of passesAllChecks="+passesAllChecks)
 
         if(passesAllChecks==false){
-          console.log("END of GUI. passesAllChecks="+passesAllChecks)
-            console.log("\nTry again\n");
+            console.log("")
+            console.log("\nYour password doesn't work. Try again\n");
             password.close();
             getString();
         }
         else{
-              console.log("END of GUI. passesAllChecks="+passesAllChecks)
+              console.log("")
             console.log("\nGreat password. Bye bye now...\n");
             password.close();
         }
