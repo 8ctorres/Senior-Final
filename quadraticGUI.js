42dfd91dc8ec8d0233e0b8bdc590aa1db7e9e51d
Carlos Torres <8ctorres@stpatrick.org>
2017-05-14 10:43:59 -0500

    Changes

--- quadraticGUI.js
+++ quadraticGUI.js
@@ -13,21 +13,23 @@ function getString(){
 
         quadratic.question("What is your 'a' value?", function(str){
           a = str;
+          quadratic.question("What is your 'b' value?", function(str){
+            b = str;
+            quadratic.question("What is your 'c' value?", function(str){
+             c = str;
 
+             let x1 = calc.equation1(a,b,c);
+             let x2 = calc.equation2(a,b,c);
+
+             console.log("X equals "+x1+" and "+x2+" .")
+             
+             });
+          });
         });
-        quadratic.question("What is your 'b' value?", function(str){
-          b = str;
 
-        });
-        quadratic.question("What is your 'c' value?", function(str){
-          c = str;
-          
-        });
 
-        let x1 = calc.equation1(a,b,c);
-        let x2 = calc.equation2(a,b,c);
 
-        console.log("X equals "+x1+" and "+x2+" .")
+
 }
 
 getString();
